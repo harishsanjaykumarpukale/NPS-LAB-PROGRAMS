4c3a09de0254746f77fce5a173f982e52c7e80fc
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2021-01-14 20:30:12 +0530

    Add files via upload

--- 3-prg/hamming.c
+++ 3-prg/hamming.c
@@ -1,57 +1,65 @@
 #include<stdio.h>
 
 void main() {
-    int data[10];
-    int dataatrec[10],c,c1,c2,c3,i;
+    int data[8],recv[8],c,c1,c2,c3,i,t;
 
     printf("Sender: ");
-    scanf("%d %d %d %d",&data[0] ,&data[1] ,&data[2] ,&data[4]);
+    scanf("%d %d %d %d",&data[3] ,&data[5] ,&data[6] ,&data[7]);
 
-    //Calculation of even parity
-    data[6]=data[0]^data[2]^data[4];
-    data[5]=data[0]^data[1]^data[4];
-    data[3]=data[0]^data[1]^data[2];
+    //XOR of bits having 1 in first place
+    data[1]=data[3]^data[5]^data[7];
+    //XOR of bits having 1 in second place
+    data[2]=data[3]^data[6]^data[7];
+    //XOR of bits having 1 in third place
+    data[4]=data[5]^data[6]^data[7];
 
+    //XOR of whole block
+    data[0] = 0;
+    for(i=1;i<=7;i++) 
+        data[0] ^= data[i];
+    
     printf("Encoded Data: ");
-    for(i=0;i<7;i++)
+    for(i=0;i<8;i++)
         printf("%d",data[i]);
 
     printf("\nReceiver: ");
-    for(i=0;i<7;i++)
-        scanf("%d",&dataatrec[i]);
+    for(i=0;i<8;i++)
+        scanf("%d",&recv[i]);
 
-    c1=dataatrec[6]^dataatrec[4]^dataatrec[2]^dataatrec[0];
-    c2=dataatrec[5]^dataatrec[4]^dataatrec[1]^dataatrec[0];
-    c3=dataatrec[3]^dataatrec[2]^dataatrec[1]^dataatrec[0];
+    //Calculation of errerneous bit position
+    c1=recv[1]^recv[3]^recv[5]^recv[7];
+    c2=recv[2]^recv[3]^recv[6]^recv[7];
+    c3=recv[4]^recv[5]^recv[6]^recv[7];
     c=c3*4+c2*2+c1 ;
 
-    printf("\nSyndrome bits:  %d %d %d", c1, c2, c3);
+    //XOR of whole block
+    t=0;
+    for(i=0;i<8;i++)
+        t^=recv[i];
+    
 
-    if(c==0) {
+    if(c==0 && t==0) 
         printf("\nNo error while transmission of data\n");
-    }
+    else if (c!=0 && t==0)
+        printf("\nERROR detected in atleast 2 bits.\n");
     else {
+        printf("\nSyndrome bits:  %d %d %d", c3, c2, c1);
         printf("\nError Position: %d",c);
-
         printf("\nData sent: ");
-        for(i=0;i<7;i++)
+        for(i=0;i<8;i++)
             printf("%d",data[i]);
-
         printf("\nData received: ");
-        for(i=0;i<7;i++)
-            printf("%d",dataatrec[i]);
-
+        for(i=0;i<8;i++)
+            printf("%d",recv[i]);
         printf("\nCorrect message is: ");
-
         //if errorneous bit is 0 we complement it else vice versa
-        if(dataatrec[7-c]==0)
-            dataatrec[7-c]=1;
+        if(recv[c]==0)
+            recv[c]=1;
         else
-            dataatrec[7-c]=0;
-
-        for (i=0;i<7;i++) {
-            printf("%d",dataatrec[i]);
-        }
+            recv[c]=0;
+        for (i=0;i<8;i++)
+            printf("%d",recv[i]);
+        printf("\n");
     }
 }
 
